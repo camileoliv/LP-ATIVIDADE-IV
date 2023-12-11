df4e6ef8d3ebfddd2d8d93b4eba78b56072217ac
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-12-11 17:04:47 -0300

    Update Atividade 3.c
    Nao finalizado

--- Atividade 3.c
+++ Atividade 3.c
@@ -8,18 +8,16 @@
 
 struct dados_produto
 {
-    char Xiaomi, Samsung, Motorola;
-    int estoXiaomi = 10, 
-    float xiaomiPrec = 1.400;
+    char nome;
+    float preco;
+    int estoque;
 };
 
-void atualizarEstoque (struct dados_produto produto, ) {
- if ()
- {
-    
- }
- 
-
+void atualizarEstoque(struct dados_produto produto, int estoXiaomi, char *produtoNome)
+{
+    if (strcmp(produtoNome, ))
+    {
+    }
 }
 
 int main()
@@ -31,61 +29,62 @@ int main()
 
     do
     {
-        printf("Lojinha da Xuxa\n");
-        printf("1 - Realizar uma compra\n");
-        printf("2 - Consultar estoque\n");
-        printf("3 - Sair do programa.\n");
-        printf("Resposta: ");
-        scanf("%i", &opcao);
+        printf("Registre o seu produto.\n");
+        printf("\n");
 
-        system("cls||clear");
+    } while ();
 
-        fflush(stdin);
 
-        switch (opcao)
+    printf("Lojinha da Xuxa\n");
+    printf("1 - Realizar uma compra\n");
+    printf("2 - Consultar estoque\n");
+    printf("3 - Sair do programa.\n");
+    printf("Resposta: ");
+    scanf("%i", &opcao);
+
+    system("cls||clear");
+
+    fflush(stdin);
+
+    switch (opcao)
+    {
+    case 1:
+        printf("Insira dados do produto");
+        printf("\n");
+
+        printf("Nome do produto: \n");
+        gets(produtoNome);
+
+        if (strcmp(produto[i].nome, produtoNome) == 0)
         {
-        case 1:
-            printf("Insira dados do produto");
-            printf("\n");
-
-            printf("Nome do produto: \n");
-            gets(produtoNome);
-
-            if (strcmp(produto[i].nome, produtoNome) == 0)
-            {
-                printf("Valor do produto: %.1f \n", produto[i].preco);
-                break;
-            }
-            printf("\nProduto não encontrado.\n");
-
-            atualizarEstoque();
-
-            break;
-
-        case 2:
-            printf("Produtos.\n");
-            printf("Celular Xiaomi.");
-            printf("Preço: %.2f\n",xiaomiPrec);
-            printf("Quantidade: ");
-
-            printf("Celular Samsung.");
-            printf("Preço: %.2f\n",samsungPrec);
-            printf("Quantidade: ");
-
-            printf("Celular Motorola.");
-            printf("Preço: %.2f\n",motorolaPrec);
-            printf("Quantidade: ");
-
-            break;
-        case 3:
-
-            break;
-
-        default:
+            printf("Valor do produto: %.1f \n", produto[i].preco);
             break;
         }
+        printf("\nProduto não encontrado.\n");
 
-    } while (opcao);
+        atualizarEstoque();
+
+        break;
+
+    case 2:
+        printf("Produtos.\n");
+        printf("Celular Xiaomi.");
+        // printf("Preço: %.2f\n",xiaomiPrec);
+        printf("Quantidade: ");
+
+        printf("Celular Samsung.");
+        // printf("Preço: %.2f\n",samsungPrec);
+        printf("Quantidade: ");
+
+        printf("Celular Motorola.");
+        // printf("Preço: %.2f\n",motorolaPrec);
+        printf("Quantidade: ");
+
+        break;
+
+    default:
+        break;
+    }
 
     return 0;
 }
