53a9daf7d8c11489b8e8f027aaad6c50a6efac33
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2023-12-12 10:31:48 -0300

    Add files via upload

---
+++ Atividade 5.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAM 250
+
+struct informacao
+{
+    char nome[200], cargo[200];
+    float salario;
+};
+
+float media(struct informacao funcionario[])
+{
+    int i, j;
+    float soma, resultado;
+
+    for (j = 0; j < TAM; j++)
+    {
+        if (strcmp(funcionario[i].cargo, "Desenvolvedor") == 0)
+        {
+            j++;
+            soma += funcionario[i].salario;
+        }
+    }
+    resultado = soma / (float)j;
+    return resultado;
+}
+
+int main()
+{
+
+    int opcao, i;
+    
+    struct informacao funcionario[TAM];
+
+    do
+    {
+        printf("App Empresa\n");
+        printf(" 1 - Adicionar informação\n");
+        printf(" 2 - Exibir\n");
+        printf(" 3 - Sair do programa\n");
+        printf("Resposta: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            setbuf(stdin, 0);
+
+            printf("Digite o nome do %dº funcionário: ", i + 1);
+            gets(funcionario[i].nome);
+
+            printf("Digite o nome do cargo: ");
+            gets(funcionario[i].cargo);
+
+            printf("Digite o valor do salario: ");
+            scanf("%f", &funcionario[i].salario);
+            i++;
+
+            system("cls||clear");
+
+            break;
+        case 2:
+            printf("Média salarial: R$ %.2f\n", media(funcionario));
+            break;
+        }
+
+    } while (opcao != 3);
+
+    return 0;
+}
